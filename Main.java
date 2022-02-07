bb05f174cbbe5410ce647b5a6cd7b30985e488c2
Burak Ünal <82771124+Melanckor@users.noreply.github.com>
2022-02-07 19:47:13 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main (String[] args){
 		System.out.println("Fuck you");
-		System.out.println("Fuck you two times");
+		System.out.println("Fuck you two times2");
 
 	}
 

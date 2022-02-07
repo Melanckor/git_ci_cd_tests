d9e2e5045e03149c094966c3a0650cf813c5cd3c
Burak Ünal <burak.unal.ac@hotmail.com>
2022-02-07 20:40:33 +0100

    Fuck

--- demo-1/src/main/java/com/example/demo/Demo1Application.java
+++ demo-1/src/main/java/com/example/demo/Demo1Application.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Demo1Application {
 
 	public static void main(String[] args) {
+		System.out.println("start the fuck");
 		SpringApplication.run(Demo1Application.class, args);
+		
 	}
 
 }
